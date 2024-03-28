928bf7291653ac94e31f1bf34708c3668cde950e
MaksItLab <maksim.ardintsev@mail.ru>
2024-03-28 23:02:51 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+using System.Text;
+
+
+namespace ConsoleApp1
+{
+	internal class Program
+	{
+		static Char[] chars = "0123456789abcdefghijklmnopqrstuvwxyz".ToCharArray();
+
+		static string generatePasssword(int len)
+		{
+			Random random = new Random();
+			StringBuilder pass = new StringBuilder();
+
+			for (int i = 0; i < len; i++)
+			{
+				int index = random.Next(chars.Length);
+				pass.Append(chars[index]);
+			}
+			return pass.ToString();
+
+		}
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Сколько паролей надо сгенирировать");
+			int count = int.Parse(Console.ReadLine());
+			Console.WriteLine("Укажите длину пароля");
+			int len = int.Parse(Console.ReadLine());
+			for (int i = 0; i < count; i++)
+			{
+				string password = generatePasssword(len);
+				Console.WriteLine(password);
+			}
+
+		}
+	}
+}
